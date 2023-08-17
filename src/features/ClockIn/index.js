a543aeebd6cd95199208fcd12644c96e261610c7
Xhuljo Balli <xh.balliu@codevider.com>
2023-08-17 02:58:18 +0200

    fix

--- src/features/ClockIn/index.js
+++ src/features/ClockIn/index.js
@@ -2,6 +2,7 @@ import { useCallback, useState } from "react";
 import useSWR from "swr";
 import useSWRMutation from "swr/mutation";
 import Select from "../../components/Inputs/Select";
+import useApi from "../../utils/api";
 import { renderSetting } from "../../utils/helpers/settings";
 import { useUser } from "../../utils/hooks/useUser";
 
@@ -10,18 +11,21 @@ const clocks = ["start-shift", "end-shift", "start-break", "end-break"];
 function ClockIn() {
     const user = useUser();
 
-    const [job, setJob] = useState(null);
-    const [location, setLocation] = useState(null);
-    const [project, setProject] = useState(null);
+    const [job, setJob] = useState(user.jobs?.[0]?.id);
+    const [location, setLocation] = useState(user.locations?.[0]?.id);
+    const [project, setProject] = useState(user.projects?.[0]?.id);
 
     const { data, isLoading } = useSWR({
         url: "/clock"
     });
+    const { authPost } = useApi();
     const { trigger, isMutating } = useSWRMutation(
         "/clock",
         useCallback((url, { arg }) => {
-            console.log({ url, arg, job, location, project });
-        }, [job, location, project]),
+            const data = { type: arg, job, location, project };
+            return authPost(url, { data });
+        }, [authPost, job, location, project]),
+        { revalidate: true },
     );
 
     console.log(data);
